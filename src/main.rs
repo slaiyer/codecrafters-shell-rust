b948fd84e6c1ec8b6e7afadcc8b6316b244aaa97
slaiyer <1776970+slaiyer@users.noreply.github.com>
2024-05-26 14:46:14 +0530

    implement command not found

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,33 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    repl_start();
+}
 
-    // Wait for user input
+fn repl_start() {
+    let mut stdout = io::stdout();
     let stdin = io::stdin();
+
+    loop {
+        prompt_print(&mut stdout);
+        let input = input_read(&stdin);
+        if let Err(e) = command_parse(&input) {
+            println!("{e}")
+        }
+    }
+}
+
+fn command_parse(input: &str) -> Result<String, String> {
+    Err(format!("{input}: command not found"))
+}
+
+fn input_read(stdin: &io::Stdin) -> String {
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    input.trim().to_owned()
+}
+
+fn prompt_print(stdout: &mut io::Stdout) {
+    print!("$ ");
+    stdout.flush().unwrap();
 }
